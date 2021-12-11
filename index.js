849ec24412adc53eee537dac300e9bf403b43062
viper23 <htonnycarlos@gmail.com>
2021-12-11 11:24:29 +0200

    finish js code

--- index.js
+++ index.js
@@ -7,14 +7,23 @@ const dataLowercase = "abcdefghijklmnopqrstuvwxyz"
 const dataUppercase = dataLowercase.toUpperCase();
 const dataNumbers = "0123456789";
 const dataSymbols = "?.;/:!§ù%$£¨@&~#\"(=)";
+const rangeValue = document.getElementById("password-length");
+const passwordOutput = document.getElementById("password-output");
 
 function generatePassword (){
     let data = [];
-    if (lowercase.checked) data.push(dataLowercase);
-    if (uppercase.checked) data.push(dataUppercase);
-    if (numbers.checked) data.push(dataNumbers);
-    if (symbols.checked) data.push(dataSymbols);
-    console.log(data);
+    let passwordGenearated = "";
+
+    if (lowercase.checked) data.push(...dataLowercase);
+    if (uppercase.checked) data.push(...dataUppercase);
+    if (numbers.checked) data.push(...dataNumbers);
+    if (symbols.checked) data.push(...dataSymbols);
+    if (data.length ===0) {alert("You must choose one of conditions'password generation"); return;}
+    
+    for (i = 0; i < rangeValue.value; ++i) {
+        passwordGenearated += (data[Math.floor(Math.random() * data.length)]);     
+    }
+    passwordOutput.value = passwordGenearated;
 };
 
 generateButton.addEventListener("click", ()=>{
